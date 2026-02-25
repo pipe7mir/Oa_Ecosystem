029773b146e9bd739a56bd3509035c65eb898e58
Pipe Mila <felipemiranda959@gmail.com>
2026-02-25 17:57:21 -0500

    fix(client): set API baseURL to relative /api for production and proxy dev requests

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,12 @@ export default defineConfig({
     port: 5173,
     strictPort: false,
     open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      }
+    }
   },
   build: {
     outDir: 'dist/client',
